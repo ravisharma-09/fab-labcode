62c80d169b08c375ecec548dd6561f5263abcea0
ravisharma-09 <sharmaravi75080@gmail.com>
2026-05-06 14:44:47 +0530

    folderstructure

---
+++ FAB-LABCODE/src/main.jsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './style.css'
+createRoot(document.getElementById('root')).render(
+  <> <div class="nav">
+  <h1>FAB-LabCODe</h1> <button class="Gcode">G-code  </button>
+  </div>
+  <div class="main">
+    <div class = "chat">
+
+    </div>
+    <div class = "code">
+
+    </div>
+  </div>
+  </>
+)
